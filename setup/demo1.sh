9a88dde4a27f83218786cb422135eb9a0629df36
Rick Armstrong <rickatech@c7x2ip78.local.zaptech.org>
2017-03-19 15:06:04 -0400

    added -mt, -mb and other options to make installation more customizable

--- setup/demo1.sh
+++ setup/demo1.sh
@@ -1,11 +1,76 @@
-# directory of mytake repository
-MYTAKE=mytake
-# document root of website
 SITE=site
 # local storage directory, works well for small sites and demos
-BOG=bog
+
+# directory of mytake repository
+MYTAKE=mytake
+MYTAKEP=../
+# document root of website
+# directoory of MobileDetect
+MOBDET=MobileDetect
+MOBDETP=../
+# directoory of php-markdown
+MKDOWN=php-markdown
+MKDOWNP=../
+
+# local storage directory, works well for small sites and demos
+BOG=$(pwd)/bog
+# BOG=bog
+
 # local storage directory, directory group owner (e.g. apache, httpd, webapp)
 WEBGRP=blog
+
+# CITE
+# http://stackoverflow.com/questions/7069682/how-to-get-arguments-with-flags-in-bash-script
+while test $# -gt 0; do
+  case "$1" in
+    -mb)
+      shift
+      if test $# -gt 0; then
+        MOBDET=$1
+        MOBDETP=
+      else
+        HELP="no -mb path specified"
+        MTERR=$HELP
+      fi
+      shift
+      ;;
+    -md)
+      shift
+      if test $# -gt 0; then
+        MKDOWN=$1
+        MKDOWNP=
+      else
+        HELP="no -md path specified"
+        MTERR=$HELP
+      fi
+      shift
+      ;;
+    -mt)
+      shift
+      if test $# -gt 0; then
+        MYTAKE=$1
+        MYTAKEP=
+      else
+        HELP="no -mt path specified"
+        MTERR=$HELP
+      fi
+      shift
+      ;;
+    -bg)
+      shift
+      if test $# -gt 0; then
+        BOG=$1
+      fi
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+# user file upload directory
+UPL=$(pwd)/$SITE/gfx-upload
 
 MTERR="mytake directory not found"
 if [ ! -d "$MYTAKE" ]
@@ -25,19 +90,19 @@ fi
 
 if [ ! -v HELP ]
 then
-  MTERR="Mobile-Detect directory not found"
-  if [ ! -d "Mobile-Detect" ]
+  MTERR="$MOBDET directory not found"
+  if [ ! -d $MOBDET ]
   then
-    HELP="ak no Mobile-Detect"
+    HELP="ak no $MOBDET"
   fi
 fi
 
 if [ ! -v HELP ]
 then
-  MTERR="php-markdown directory not found"
-  if [ ! -d "php-markdown" ]
+  MTERR="$MKDOWN directory not found"
+  if [ ! -d $MKDOWN ]
   then
-    HELP="ak no php-markdown"
+    HELP="ak no $MKDOWN"
   fi
 fi
 
@@ -62,10 +127,30 @@ then
   MTERR="site directory not found"
   if [ -d "$SITE" ]
   then
-      ln -s ../$MYTAKE                      $SITE/$MYTAKE
-      ln -s ../Mobile-Detect                $SITE/mobile_detect
-      ln -s ../php-markdown                 $SITE/markdown
-      cp mytake/setup/config.php            $SITE
+      if [ -d "$SITE/mytake" ]
+      then
+      rm -f $SITE/mytake
+      fi
+      ln -s $MYTAKEP$MYTAKE                 $SITE/mytake
+
+      if [ -d "$SITE/mobile_detect" ]
+      then
+      rm -f $SITE/mobile_detect
+      fi
+      ln -s $MOBDETP$MOBDET                 $SITE/mobile_detect
+
+      if [ -d "$SITE/markdown" ]
+      then
+      rm -f $SITE/markdown
+      fi
+      ln -s $MKDOWNP$MKDOWN                 $SITE/markdown
+
+      # insert local storage directory, determined dynamically
+      sed s:##CWDBOG##:$BOG: $MYTAKE/setup/config_sample.php >$SITE/config_sample.php0
+      sed s:##FILUPL##:$UPL: $SITE/config_sample.php0 >$SITE/config.php
+      rm -f $SITE/config_sample.php0
+#     cp mytake/setup/config.php            $SITE
+
       cp mytake/setup/index.php             $SITE
       cp mytake/setup/util.php              $SITE
       cp mytake/setup/base.*                $SITE
@@ -92,10 +177,24 @@ if [ -v HELP ]
 then
     echo "Error: "$MTERR
     echo 
-    echo "[ to reset: rm -fr bog; rm -fr site/* ]"
-else
-    echo "Done, now update URL and file paths in config.php!"
     echo 
-    echo "[ to reset: rm -fr bog; rm -fr site/* ]"
+    echo "options"
+    echo "-mt path     override mytake files path     (e.g. /public/mytake)"
+    echo "-mb path     override path to Mobile-Detect (e.g. /public/Mobile-Detect)"
+    echo "-md path     override path to php_markdon   (e.g. /public/php-markdown)"
+    echo "-bg path     override private file store    (e.g. /public/bog)"
+    echo
+    echo "Example"
+    echo "sh mytake/setup/demo1.sh -mb /public/Mobile-Detect -md /public/php-markdown"
+else
+    echo 
+    echo "Done, now update URL and file paths in config.php!"
+    echo "  \$actv_url ="
+    echo "  \$data_dir = "
+    echo "  \$file_dir = "
+
 fi
+
+echo 
+echo "[ to reset: rm -fr "$BOG"; rm -fr "$SITE"/* ]"
 
