bd0ad13c4f35583bf6c08e1dadfd8b504b94d055
Rick Armstrong <rickatech@c7x2ip78.local.zaptech.org>
2017-03-24 01:40:27 -0400

    dropped $foot, setup/panel/footer_static.php

---
+++ setup/panel/footer_static.php
@@ -0,0 +1,7 @@
+<div style="text-align: center; font-family: sans-serif; margin-top: 0.5em;">
+about | join | promotions | careers
+</div> 
+
+<?PHP
+if ($debug_mask & DEBUG_DISPSIZE)
+	echo "<div style=\"position: fixed; bottom: 0; right: 0; background-color: white; text-align: right; font-size: smaller;\">".$notes."<span id='mt_msg'></span></div>";
